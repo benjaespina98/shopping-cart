15e7bb24a660fee181f430a35d74e10013d2c27e
benjaespina98 <83318957+benjaespina98@users.noreply.github.com>
2026-06-26 11:50:34 -0300

    Admin updates

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import { login, getMe, changePassword } from '../controllers/authController.js';
+import { login, getMe } from '../controllers/authController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
 router.post('/login', login);
 router.get('/me', protect, getMe);
-// endpoint deshabilitado por seguridad a petición
-// router.put('/change-password', protect, changePassword);
 
 export default router;
